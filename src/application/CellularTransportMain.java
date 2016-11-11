f70435d81baf86e08f907caab06412c5e7b4fb66
Stefn93 <stefan.romanazzi@gmail.com>
2016-11-11 14:23:47 +0100

    Fixes dovuti

--- src/application/CellularTransportMain.java
+++ src/application/CellularTransportMain.java
@@ -4,7 +4,7 @@ import cellularTransport.CTBuilder;
 import cellularTransport.CTStateList;
 import fcast.application.SimulationApplication;
 
-public class CellularMembraneMain extends SimulationApplication {
+public class CellularTransportMain extends SimulationApplication {
 	
 	public static void main(String[] args) {
 		setSimulation(new CTBuilder(), new CTStateList());
